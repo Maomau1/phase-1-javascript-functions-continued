9a3969c7471eecbfeddc6e43acf6376229b987fc
maurice diong <mdiong00@gmail.com>
2025-05-23 01:59:05 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(par='*'){
+    return (function(par2='special'){
+        return `You are ${par+par2+par}!`
+    })
+}
